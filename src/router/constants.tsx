90fa423a1c40aea689ab99cdd6664e045b119065
Mondrian92 <m.maugeri92@gmail.com>
2025-11-17 23:23:30 +0100

    fix: statistics page route path

--- src/router/constants.tsx
+++ src/router/constants.tsx
@@ -43,7 +43,7 @@ export const navbarRoutes: Route[] = [
     name: "home",
   },
   {
-    path: "/crops_costs",
+    path: "/statistics",
     element: <Statistics />,
     name: "statistics",
   },
